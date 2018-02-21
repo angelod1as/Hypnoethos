6f33f7aa35fa038ff4f2a9b400b552db2b37feb6
angelod1as <angelo@angelodias.com.br>
2018-02-21 09:09:39 -0300

    put jquery as external resource

--- others/boilerplate/src/assets/script/main.js
+++ others/boilerplate/src/assets/script/main.js
@@ -1,9 +1,9 @@
-import jQuery from 'jquery';
+// import jQuery from 'jquery';
 // import buttonScroll from './components/buttonScroll';
 
 // export for others scripts to use
-window.$ = jQuery;
-window.jQuery = jQuery;
+// window.$ = jQuery;
+// window.jQuery = jQuery;
 
 $(document).ready(() => {
 	$('.modal__btn').on('click', () => {
